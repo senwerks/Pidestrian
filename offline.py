4d281a6e1e502c0e5baf0e200277eb85f1ea551f
Sen <sen@senwerks.com>
2023-12-28 14:56:53 +1100

    Simplify.

--- offline.py
+++ offline.py
@@ -1,22 +1,22 @@
 ################################################################
 # Pedestrian Lights Controller by Sen (Offline Version)
-# v 0.1 (simply simple)
+# v 0.2 (simply simplier)
 # https://github.com/senwerks/pedestrian-lights
 #
 # TODO: Actually test this file, it's only been run in my head
 ################################################################
 
-from machine import Pin, Timer
+from machine import Pin
 import time
 
 # Timer Period - Edit this to change "Green" time.
-timer_period = 1000 # Set to 120000 when Live
+timer_period = 180 # Set to 180 when Live
 
 # Set up the 2 relay pins
-relay1 = Pin(27, Pin.OUT)
-relay1.value(0)
-relay2 = Pin(28, Pin.OUT)
-relay2.value(0)
+green_light = Pin(27, Pin.OUT)
+green_light.value(0)
+red_light = Pin(28, Pin.OUT)
+red_light.value(0)
 
 # Set up the physical button
 pin = Pin(22, Pin.IN, Pin.PULL_UP)
@@ -25,51 +25,43 @@ pin = Pin(22, Pin.IN, Pin.PULL_UP)
 interrupt_flag = 0
 debounce_time = 0
 
-# Ghetto State Machine (most aren't being used yet but are for future plans)
+# Ghetto State Machine
 state = "Off"
-states = ["Off", "On", "Go", "Stop", "Timer"]
-
-relay1_timer = Timer(-1)
-relay2_timer = Timer(-1)
+states = ["Go", "Flash", "Stop", "Timer", "Boot"]
 
 def change_state(state):
-    global relay1, relay2, relay1_timer, relay2_timer
-    if state == "Off":
-        relay1.value(0)
-        relay2.value(0)
-        relay1_timer.deinit()
-        relay2_timer.deinit()
-    elif state == "On":
-        relay1.value(1)
-        relay2.value(1)
-        relay1_timer.deinit()
-        relay2_timer.deinit()
-    elif state == "Go":
-        relay1.value(1)
-        relay2.value(0)
-        relay1_timer.deinit()
-        relay2_timer.deinit()
+    print('Changed to %s State' % state)
+    global green_light, red_light
+    
+    if state == "Go":
+        green_light.value(1)
+        red_light.value(0)
     elif state == "Flash":
-        relay1_timer.deinit()
-        relay2_timer.deinit()
+        green_light.value(0)
         for i in range(10):
-            relay2.value(1)
-            time.sleep(0.25)
-            relay2.value(0)
-            time.sleep(0.25)
+            red_light.value(1)
+            time.sleep(0.5)
+            red_light.value(0)
+            time.sleep(0.5)
         change_state("Stop")
     elif state == "Stop":
-        relay1.value(0)
-        relay2.value(1)
-        relay1_timer.deinit()
-        relay2_timer.deinit()
+        green_light.value(0)
+        red_light.value(1)
     elif state == "Timer":
-        relay1.value(1)
-        relay2.value(0)
-        relay1_timer.deinit()
-        relay2_timer.deinit()
-        relay1_timer.init(mode=Timer.ONE_SHOT, period=timer_period, callback=lambda t: change_state("Flash"))
-
+        green_light.value(1)
+        red_light.value(0)
+        time.sleep(timer_period)
+        change_state("Flash")
+    elif state == "Boot":
+        for i in range(5):
+            red_light.value(1)
+            green_light.value(1)
+            time.sleep(0.2)
+            red_light.value(0)
+            green_light.value(0)
+            time.sleep(0.2)
+        change_state("Stop")
+    
 def callback(pin):
     global interrupt_flag, debounce_time
     if (time.ticks_ms() - debounce_time) > 500:
@@ -79,13 +71,15 @@ def callback(pin):
 pin.irq(trigger=Pin.IRQ_FALLING, handler=callback)
 
 # Time to put it all together...
+change_state("Boot")
 
 while True:
     if interrupt_flag == 1:
         interrupt_flag = 0
         print("Button Pressed!")
-        state = states[(states.index(state) + 1) % len(states)]
-        change_state(state)
-        print("State:", state)
-        
+        change_state("Timer")
+        # or rotate through available States:
+        # state = states[(states.index(state) + 1) % len(states)]
+        #change_state(state)
     time.sleep(0.1)  # Add a small delay to avoid high CPU usage
+
